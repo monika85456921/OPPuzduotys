d80cb4f230f99ded45c34c950a4af9950d660575
Gerulis Donatas <okonecnikovaite.monika@gmail.com>
2024-01-17 13:39:09 +0200

    antras

--- 4skaidre/Modules/animals.js
+++ 4skaidre/Modules/animals.js
@@ -7,21 +7,21 @@ export class Animal {
 }
 
 export class Bird extends Animal {
-  constructor(name, age, wings, feathers, beak) {
+  constructor(name, age, legs, wings, feathers, beak) {
     super(name, age, legs);
     (this.wings = wings), (this.feathers = feathers), (this.beak = beak);
   }
 }
 
 export class Cat extends Animal {
-  constructor(name, age, fur, tail, whiskers) {
+  constructor(name, age, legs, fur, tail, whiskers) {
     super(name, age, legs);
     (this.fur = fur), (this.tail = tail), (this.whiskers = whiskers);
   }
 }
 
 export class Fish extends Animal {
-  constructor(name, age, fins, scales, whiskers) {
+  constructor(name, age, legs, fins, scales, whiskers) {
     super(name, age, legs);
     (this.fins = fins), (this.scales = scales), (this.whiskers = whiskers);
   }
